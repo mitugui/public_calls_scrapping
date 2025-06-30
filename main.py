674f3e01688613f8499378e7700555ba32a7da1d
mitugui <cristian.mitugui04@gmail.com>
2025-06-30 14:27:30 -0300

    feat: create base Flask API in main.py - Created Flask app with basic GET /calls route. - Enabled CORS headers using flask-cors.

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/calls', methods=['GET'])
+def get():
+    pass
+
 if __name__ == '__main__':
-    print('Hello World!')
+    app.run(debug=True)
