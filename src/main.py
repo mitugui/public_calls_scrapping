13f457887fb083958ce350cebd8fc954a84d4cfb
mitugui <cristian.mitugui04@gmail.com>
2025-08-07 14:33:37 -0300

    feat: implement GET endpoint to retrieve and paginate call data from JSON files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from scrappers.fundacao_araucaria.scrapper import FundacaoAraucariaScrapper
 from scrappers.cnpq.scrapper import CNPQScrapper
 from services.call_generator import CallGenerator
+import json
 
 app = Flask(__name__)
 
@@ -11,8 +12,31 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/calls', methods=['GET'])
-def get():
-    pass
+def get():    
+    path = './fundacao_araucaria_calls.json'
+
+    with open(path, 'r') as file:
+        data = json.load(file)
+
+    path = './cnpq_calls.json'
+
+    with open(path, 'r') as file:
+        data += json.load(file)
+    
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 10, type=int)
+
+    start = (page - 1) * per_page
+    end = start + per_page
+
+    paginated_data = data[start:end]
+
+    return {
+        'page': page,
+        'per_page': per_page,
+        'data': paginated_data,
+        'total': len(data),
+    }, 200
 
 if __name__ == '__main__':
     CallGenerator.generate(
